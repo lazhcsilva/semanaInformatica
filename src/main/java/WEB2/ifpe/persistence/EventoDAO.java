ac09004ed012d256858d985e541a2763b799e138
gabriel lima <gabriel.luiz.g.lima@accenture.com>
2020-01-19 09:01:59 -0300

    validacao de palestra

--- src/main/java/WEB2/ifpe/persistence/EventoDAO.java
+++ src/main/java/WEB2/ifpe/persistence/EventoDAO.java
@@ -1,10 +1,16 @@
 package WEB2.ifpe.persistence;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import WEB2.ifpe.model.Evento;
+import WEB2.ifpe.model.Sala;
 
 public interface EventoDAO extends JpaRepository<Evento, Integer> {
 
 	Evento findByNomeAtividade(String nomeAtividade);
+	
+	List<Evento> findByDataAndSala(LocalDate data, Integer idSala);
 }
--- src/main/java/WEB2/ifpe/service/EventoService.java
+++ src/main/java/WEB2/ifpe/service/EventoService.java
@@ -42,17 +42,25 @@ public class EventoService {
 	public void checarRegras(Evento evento) throws ServiceException {
 		
 		/**verificar se o nome da atividade já existe**/
-		Evento eventoBusca = eventoDAO.findByNomeAtividade(evento.getNomeAtividade()); 
+		Evento eventoBuscaByName = eventoDAO.findByNomeAtividade(evento.getNomeAtividade()); 
 		
-		if(eventoBusca != null) {
+		if(eventoBuscaByName != null) {
 			throw new ServiceException("Já existe um evento com esse nome");
 		}
 		
-//		/**verifica se existe uma sala usada no horário**/
-//		if(eventoBusca.getSala().getNumero().equals(evento.getSala().getNumero())
-//				&& eventoBusca.get) {
-//			
-//		}
+		/**verifica o inio e o fim da palestra**/
+		if(evento.getHoraInicio().isAfter(evento.getHoraFim())) {
+			throw new ServiceException("Hora do início do evento não pode ser maior que o horário do fim");
+		}
+		
+		/**verifica se já existe uma palestra em dado horário**/
+		List<Evento> eventoByDate = eventoDAO.findByDataAndSala(evento.getData(), evento.getSala().getIdSala());
+		for(Evento eventoIndex: eventoByDate ) {
+			if(eventoIndex.getHoraInicio().isAfter(evento.getHoraInicio()) && eventoIndex.getHoraInicio().isBefore(evento.getHoraFim())
+					|| eventoIndex.getHoraFim().isAfter(evento.getHoraInicio()) && eventoIndex.getHoraFim().isBefore(evento.getHoraFim())) {
+				throw new ServiceException("Já existe uma palestra reservada nesse horário");
+			}
+		}
 			
 	}
 	
